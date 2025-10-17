c7397f8fd78017eb7adc61591821e2ee1e80e1e4
EderMoros3 <edermorosrodero@gmail.com>
2025-10-17 11:18:16 +0200

    db impement

---
+++ crud/db/db.js
@@ -0,0 +1,22 @@
+import pg from 'pg';
+
+const { Pool } = pg;
+
+// Pool:
+// Cursor de la base de datos, la conexion a la db.
+// Configuracion de la conexion
+// es un json con los datos de conexion
+// Puedes tener varias pools en una app
+// como hacer microservicios
+
+const config = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: 'granja-abejas',
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+};
+
+const pool = new Pool(config);
+
+export default pool;
