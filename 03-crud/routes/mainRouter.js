56152c1a8ceb06421e380b1e1ec35d09af4ca731
EderMoros3 <edermorosrodero@gmail.com>
2025-10-17 17:17:20 +0200

    fix get

--- 03-crud/routes/mainRouter.js
+++ 03-crud/routes/mainRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import clientesRoutes from './clientes.routes.js';
+import pedidosRoutes from './pedidos.routes.js';
 
 const router = Router();
 
 router.use(clientesRoutes);
-
+router.use(pedidosRoutes);
 
 
 
---
+++ 03-crud/services/pedidos.service.js
@@ -0,0 +1,7 @@
+import pool from "../db/db.js";
+
+export const getAllPedidos = async () => {
+  const result = await pool.query('SELECT * FROM pedidos');
+  console.log(result.rows);
+  return result.rows;
+}
